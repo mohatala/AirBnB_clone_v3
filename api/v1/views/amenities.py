84a423e576700eb18886afd77deb155576854d32
Oumayma Mazoz <91220282+omazoz@users.noreply.github.com>
2023-10-30 20:34:42 +0100

    updates

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""State objects that handles all default RESTFul API actions"""
+"""Authors : Mazoz / Talaini
+State objects that handles all default RESTFul API actions"""
 
 from api.v1.views import app_views
 from models import storage
--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -9,7 +9,7 @@ from os import environ
 STORAGE_TYPE = environ.get('HBNB_TYPE_STORAGE')
 
 
-@app_views.route('/cities/<city_id>/places', methods=['GET', 'POST'])
+@app_views.route('/cities/<city_id>/places', methods=['GET'])
 def places_per_city(city_id=None):
     """
         places route to handle http method for requested places by city
@@ -18,29 +18,29 @@ def places_per_city(city_id=None):
     if city_object is None:
         abort(404, 'Not found')
 
-    if request.method == 'GET':
-        all_places = storage.all('Place')
-        city_places = [obj.to_json() for obj in all_places.values()
-                       if obj.city_id == city_id]
-        return jsonify(city_places)
+    all_places = storage.all('Place')
+    city_places = [obj.to_json() for obj in all_places.values()
+                    if obj.city_id == city_id]
+    return jsonify(city_places)
 
-    if request.method == 'POST':
-        req_json = request.get_json()
-        if req_json is None:
-            abort(400, 'Not a JSON')
-        user_id = req_json.get("user_id")
-        if user_id is None:
-            abort(400, 'Missing user_id')
-        user_object = storage.get('User', user_id)
-        if user_object is None:
-            abort(404, 'Not found')
-        if req_json.get("name") is None:
-            abort(400, 'Missing name')
-        Place = CNC.get("Place")
-        req_json['city_id'] = city_id
-        new_object = Place(**req_json)
-        new_object.save()
-        return jsonify(new_object.to_json()), 201
+@app_views.route('/cities/<city_id>/places', methods=['POST'])
+def places_per_city(city_id=None):
+    req_json = request.get_json()
+    if req_json is None:
+        abort(400, 'Not a JSON')
+    user_id = req_json.get("user_id")
+    if user_id is None:
+        abort(400, 'Missing user_id')
+    user_object = storage.get('User', user_id)
+    if user_object is None:
+        abort(404, 'Not found')
+    if req_json.get("name") is None:
+        abort(400, 'Missing name')
+    Place = CNC.get("Place")
+    req_json['city_id'] = city_id
+    new_object = Place(**req_json)
+    new_object.save()
+    return jsonify(new_object.to_json()), 201
 
 
 @app_views.route('/places/<place_id>', methods=['GET', 'DELETE', 'PUT'])
