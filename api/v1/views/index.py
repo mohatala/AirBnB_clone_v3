feedd1b0e8a276c24dad80dc03e5d2f383197402
Talaini <talainimohammed@gmail.com>
2023-10-30 19:32:04 +0100

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,23 +5,28 @@ from models import storage
 from models.base_model import BaseModel
 from flask import jsonify
 from api.v1.views import app_views
-
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.user import User
 
 @app_views.route('/status', strict_slashes=False)
 def returnstuff():
-    '''return stuff'''
+    '''return ok'''
     return jsonify(status='OK')
 
 
 @app_views.route('/stats', strict_slashes=False)
 def stuff():
     '''JSON Response'''
-    todos = {'states': "State",
-             'users': "User",
-             'amenities': "Amenity",
-             'cities': "City",
-             'places': "Place",
-             'reviews': "Review"}
-    for key in todos:
-        todos[key] = storage.count(todos[key])
-    return jsonify(todos)
+    classes = {'states': State,
+             'users': User,
+             'amenities': Amenity,
+             'cities': City,
+             'places': Place,
+             'reviews': Review}
+    for key in classes:
+        classes[key] = storage.count(classes[key])
+    return jsonify(classes)
