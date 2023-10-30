152d64ac1480eb0dcfe39e5e03337d9b83081838
Talaini <talainimohammed@gmail.com>
2023-10-30 19:29:15 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,11 +18,11 @@ port = os.getenv('HBNB_API_PORT', 5000)
 
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
+def close_db(exception):
     """
     after each request, this method calls .close()
     """
@@ -35,51 +35,24 @@ def handle_error_404(exception):
     handles 404 errors
     """
     code = exception.__str__().split()[0]
-    description = exception.description
-    msg = {'error': description}
-    return make_response(jsonify(msg), code)
+    msg = {"error": "Not found"}
+    return jsonify(msg), code
 
 
 @app.errorhandler(400)
-def handle_error_404(exception):
+def handle_error_400(exception):
     """
     handles 400 errros
     """
     code = exception.__str__().split()[0]
     description = exception.description
     msg = {'error': description}
-    return make_response(jsonify(msg), code)
+    return jsonify(msg), code
 
-
-@app.errorhandler(Exception)
-def global_error_handler(err):
-    """
-        Global Route to handle All Error Codes
-    """
-    if isinstance(err, HTTPException):
-        if type(err).__name__ == 'NotFound':
-            err.description = "Not found"
-        msg = {'error': err.description}
-        code = err.code
-    else:
-        msg = {'error': err}
-        code = 500
-    return make_response(jsonify(msg), code)
-
-
-def setup_global_errors():
-    """
-    This updates HTTPException Class with custom error function
-    """
-    for cls in HTTPException.__subclasses__():
-        app.register_error_handler(cls, global_error_handler)
 
 
 if __name__ == "__main__":
     """
-    MAIN Flask App
+    start Flask App
     """
-    # initializes global error handling
-    setup_global_errors()
-    # start Flask app
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, threaded=True)
